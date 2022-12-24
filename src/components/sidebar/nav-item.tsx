0ea0b1eab3ed918d00e1465bb926c3b02700a187
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-24 19:12:44 +0100

    Complete NavItem component

---
+++ src/components/sidebar/nav-item.tsx
@@ -0,0 +1,71 @@
+import {Link, HStack, Icon, Text, Box, Heading} from '@chakra-ui/react'
+import NextLink from 'next/link'
+
+import {NavItem as Item} from 'types/nav-item'
+
+type Props = {
+  item: Item
+  isActive: boolean
+}
+
+const NavItem = ({isActive, item}: Props) => {
+  const {label} = item
+
+  if (item.type === 'link') {
+    const {icon} = item
+    return (
+      <NextLink href="#" passHref>
+        <Link
+          variant="unstyled"
+          _hover={{
+            textDecoration: 'none',
+          }}>
+          <HStack
+            align="center"
+            justify="flex-start"
+            height={{base: 10, '2xl': 14}}
+            transition="ease-out"
+            transitionProperty="background"
+            transitionDuration="normal"
+            _hover={{
+              background: 'gray.dark',
+            }}>
+            <Icon
+              width={5}
+              height={5}
+              mr={4}
+              ml={8}
+              color={isActive ? 'brand.red' : 'gray.light'}
+              as={icon}
+            />
+            <Text
+              fontSize="md"
+              fontWeight="medium"
+              flex={1}
+              letterSpacing="wider"
+              color={isActive ? 'brand.red' : 'whiteAlpha.900'}>
+              {label}
+            </Text>
+            {isActive && <Box width={1} height={6} bg="brand.red" />}
+          </HStack>
+        </Link>
+      </NextLink>
+    )
+  }
+
+  return (
+    <Heading
+      color="gray.light"
+      fontWeight="normal"
+      textTransform="uppercase"
+      letterSpacing={6}
+      fontSize="sm"
+      ml={8}
+      mt={{base: 6, '2xl': 8}}
+      mb={2}>
+      {label}
+    </Heading>
+  )
+}
+
+export default NavItem
