dc08f809a0427accd51aa4143f001cbb98329d84
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-24 19:15:14 +0100

    Added Sidebar component in Content component

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,9 +1,11 @@
 import {VStack, HStack} from '@chakra-ui/react'
+import {Sidebar} from 'components/sidebar'
 
 const Content = () => {
   return (
     <HStack width="full" flex={1} overflow="hidden">
       {/* Sidebar */}
+      <Sidebar />
       <VStack
         px={12}
         pt={12}
