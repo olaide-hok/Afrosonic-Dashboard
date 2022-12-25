3c081637f9ab641f925ed82454b95fd91f262eca
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-25 14:17:27 +0100

    Created TopPicks component

---
+++ src/components/top-picks/index.tsx
@@ -0,0 +1,141 @@
+import {ComponentProps, useMemo} from 'react'
+
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Heading,
+  IconButton,
+  VStack,
+  Flex,
+} from '@chakra-ui/react'
+import {FaShoppingCart} from 'react-icons/fa'
+import {Image} from 'components/image/image'
+import {useTable, Column} from 'react-table'
+import {topPicks} from './picks-data'
+import {brandRing} from 'src/utils/brandRing'
+
+const HeaderCell = ({value}: {value: string}) => (
+  <Heading size="xs" textTransform="none" color="gray.light">
+    {value}
+  </Heading>
+)
+
+export const TopPicks = (props: ComponentProps<typeof VStack>) => {
+  const data = useMemo(() => topPicks, [])
+  const columns = useMemo<Column<Pick>[]>(
+    () => [
+      {
+        accessor: 'image',
+        width: '100px',
+        Cell: ({value}) => (
+          <VStack alignItems="flex-start" justifyContent="center">
+            <Image
+              src={value}
+              alt="image"
+              width={`${parseInt('56px')}`}
+              height={`${parseInt('56px')}`}
+              minWidth="56px"
+              minHeight="56px"
+              rounded="xl"
+              objectFit="cover"
+              quality="75"
+            />
+          </VStack>
+        ),
+      },
+      {
+        Header: () => <HeaderCell value="Name" />,
+        accessor: 'name',
+      },
+      {
+        Header: () => <HeaderCell value="Producer" />,
+        accessor: 'producer',
+      },
+      {
+        Header: () => <HeaderCell value="Category" />,
+        accessor: 'category',
+      },
+      {
+        Header: () => <HeaderCell value="Time" />,
+        accessor: 'time',
+        width: '40px',
+      },
+      {
+        Header: () => <HeaderCell value="Buy" />,
+        id: 'buy',
+        width: '40px',
+        Cell: () => (
+          <IconButton
+            icon={<FaShoppingCart />}
+            aria-label="Buy Song"
+            rounded="full"
+            {...brandRing}
+          />
+        ),
+      },
+    ],
+    []
+  )
+
+  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} =
+    useTable({columns, data})
+
+  return (
+    <VStack align="flex-start" height="full" {...props}>
+      <Heading>Top Picks</Heading>
+      <Flex overflow="auto" width="full">
+        <Table {...getTableProps()} position="relative">
+          <Thead>
+            {headerGroups.map((headerGroup) => (
+              <Tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <Th
+                    width={column.width}
+                    {...column.getHeaderProps()}
+                    borderBottom={0}
+                    position="sticky"
+                    zIndex="banner"
+                    top={0}
+                    bg="black">
+                    {column.render('Header')}
+                  </Th>
+                ))}
+              </Tr>
+            ))}
+          </Thead>
+          <Tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row)
+              return (
+                <Tr
+                  {...row.getRowProps()}
+                  transition="ease-out"
+                  transitionProperty="background"
+                  transitionDuration="normal"
+                  _hover={{
+                    background: 'gray.dark',
+                    cursor: 'pointer',
+                  }}>
+                  {row.cells.map((cell) => (
+                    <Td
+                      {...cell.getCellProps()}
+                      py={2}
+                      borderBottom={0}
+                      paddingInlineEnd={0}
+                      minWidth={cell.column.width}>
+                      {cell.render('Cell')}
+                    </Td>
+                  ))}
+                </Tr>
+              )
+            })}
+          </Tbody>
+        </Table>
+      </Flex>
+    </VStack>
+  )
+}
