70d3995a7f0df6cbffe95df1d6314509f7b0b703
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-28 17:41:13 +0100

    Created Producer Component

---
+++ src/components/top-producers/producer.tsx
@@ -0,0 +1,42 @@
+import {VStack, Text, Box, useBreakpointValue} from '@chakra-ui/react'
+
+import {Image} from 'components/image/image'
+import {Producer as ProducerType} from 'types/producer'
+
+type Props = ProducerType & {
+  badge?: boolean
+}
+
+export const Producer = ({name, image, badge}: Props) => {
+  const size = useBreakpointValue({base: '48px', '2xl': '80px'}) ?? '80px'
+  return (
+    <VStack>
+      <Box position="relative">
+        <Image
+          src={image}
+          width={`${parseInt(size)}`}
+          height={`${parseInt(size)}`}
+          rounded="lg"
+          objectFit="cover"
+          alt={name}
+        />
+        {badge && (
+          <Box
+            bg="brand.red"
+            position="absolute"
+            top={-3}
+            right={-3}
+            rounded="full"
+            width={6}
+            height={6}
+            borderWidth={4}
+            borderColor="black"
+          />
+        )}
+      </Box>
+      <Text fontSize="sm" fontWeight="medium">
+        {name}
+      </Text>
+    </VStack>
+  )
+}
