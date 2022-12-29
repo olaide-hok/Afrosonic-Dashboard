18892d224be2af89d95f73404886dfd0787d11a0
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-29 16:43:50 +0100

    Created PlayerControls Component.
    Rendered the PlayerControl component

---
+++ src/components/player/player-controls.tsx
@@ -0,0 +1,17 @@
+import {HStack} from '@chakra-ui/react'
+import {Control} from 'types/control'
+import {PlayerControl} from './player-control'
+
+type Props = {
+  controls: Control[]
+}
+
+export const PlayerControls = ({controls}: Props) => {
+  return (
+    <HStack>
+      {controls.map(({label, icon}) => (
+        <PlayerControl key={label} label={label} icon={icon} />
+      ))}
+    </HStack>
+  )
+}
