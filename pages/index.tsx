20a5ff0bf3193d31fe9a073eab2bc1c6015268a5
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-29 16:47:13 +0100

    Rendered Player component in IndexPage component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import {VStack} from '@chakra-ui/react'
 import Content from 'components/content'
+import {Player} from 'components/player'
 const IndexPage = () => (
   <VStack height="100vh" bg="black" width="full" overflow="hidden" spacing={0}>
     {/* Content */}
     <Content />
     {/* Player */}
+    <Player />
   </VStack>
 )
 
