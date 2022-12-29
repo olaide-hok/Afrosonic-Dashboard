43cc54bb5cadfcffdeb778b9988f81eafe99f88e
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-29 16:36:29 +0100

    Created Disc component

---
+++ src/components/player/disc.tsx
@@ -0,0 +1,43 @@
+import {ComponentProps} from 'react'
+import {Box, Flex} from '@chakra-ui/react'
+import {motion} from 'framer-motion'
+import {Image} from 'components/image/image'
+
+const MotionBox = motion<ComponentProps<typeof Box>>(Box)
+
+export const Disc = () => {
+  return (
+    <MotionBox
+      animate={{
+        rotate: 360,
+      }}
+      transition={{
+        duration: 3,
+        repeat: Infinity,
+        type: 'tween',
+        ease: 'linear',
+      }}
+      rounded="full"
+      overflow="hidden"
+      position="relative"
+      maxW={20}
+      maxH={20}
+      width="full"
+      height="full">
+      <Image
+        src="/images/gradient-mesh.png"
+        width={80}
+        height={80}
+        objectFit="cover"
+        alt="gradient-mesh"
+      />
+      <Flex
+        pos="absolute"
+        inset="0"
+        alignItems="center"
+        justifyContent="center">
+        <Box width={6} height={6} bg="gray.dark" rounded="full" />
+      </Flex>
+    </MotionBox>
+  )
+}
