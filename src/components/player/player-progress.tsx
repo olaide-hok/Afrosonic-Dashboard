25ad3385dfd962c3528293196f389af5e87e2262
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-29 16:44:43 +0100

    Created PlayerProgress component

---
+++ src/components/player/player-progress.tsx
@@ -0,0 +1,31 @@
+import {HStack, Box, Text} from '@chakra-ui/react'
+
+export const PlayerProgress = () => {
+  return (
+    <HStack spacing={3} flex={1}>
+      <Text fontSize={12} color="gray.light">
+        01:45
+      </Text>
+      <Box
+        flex={1}
+        w="full"
+        height={1}
+        bg="gray.light"
+        position="relative"
+        rounded="full"
+        overflow="hidden">
+        <Box
+          w="75%"
+          height={1}
+          left={0}
+          insetY={0}
+          bg="brand.red"
+          rounded="full"
+        />
+      </Box>
+      <Text fontSize={12} color="gray.light">
+        04:30
+      </Text>
+    </HStack>
+  )
+}
